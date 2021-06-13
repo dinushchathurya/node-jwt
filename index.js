b19ffcfa74df9db7695d950ec9ea6a9b179cd182
dinushchathurya <dinushchathurya21@gmail.com>
2021-06-13 12:29:31 +0530

    :sparkles: create index.js file

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+require("dotenv").config();
+
+app.use(express.json());
+
+const port  = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port${port}`));
